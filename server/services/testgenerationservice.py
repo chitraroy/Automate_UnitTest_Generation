4f78d5f132d3ac1a1288d680abc135eaa93e8f45
Ankit Mehra <ankitmehra1986@gmail.com>
2024-04-08 16:00:30 -0400

    added code generation capabilities

--- server/services/testgenerationservice.py
+++ server/services/testgenerationservice.py
@@ -1,14 +1,38 @@
-import boto3
+from openai import OpenAI
+import environ
 
+env = environ.Env()
+environ.Env.read_env()
 class TestGenerationService:
     def __init__(self):
-        self.polly_client = boto3.client('polly')
-
+        self.client = OpenAI(api_key=env('OPENAI_API_KEY'))
+    
     def generate_test(self, code_block)-> dict:
         # placeholder for logic to generate test using LLM
-        generated_test = "def test_function():\n    assert True  # Placeholder for generated test"
-
+        # prompt_for_tests = f"""Write comprehensive unit tests in Python for the following
+        #   function:\n{code_block}"""
+        messages_code = [
+            {'role': 'system', 'content': 'Write comprehensive unit tests in Python for the following function:'},
+            {'role': 'user', 'content': code_block}
+        ]
+        generated_test = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=messages_code,
+        )
+        
         # placeholder for logic to get explanation from LLM
-        explanation = "This test checks if the function always returns True, which is a basic example."
+        # prompt_for_explanation = f"""Explain the following Python function and its unit tests
+        #   in simple terms:\nFunction:\n{code_block}\nGenerated Unit Tests:\n
+        #   {generated_test.choices[0].text}"""
+        message_explanation = [
+            {'role': 'system', 'content': 'Explain the following Python function and its unit tests in simple terms:'},
+            {'role': 'user', 'content': code_block},
+            {'role': 'user', 'content': generated_test.choices[0].message.content}
+        ]
+        explanation = self.client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=message_explanation
+        )
 
-        return {'test': generated_test, 'explanation': explanation}
+        return {'test': generated_test.choices[0].message.content,
+                 'explanation': explanation.choices[0].message.content}
